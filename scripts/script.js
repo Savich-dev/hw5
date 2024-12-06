1990c49b54e4a66bc412092b0bcbbc5d782c3853
SavishDev <sdmjob@mail.ru>
2024-12-06 19:02:03 +0300

    change 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,102 +1,25 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  loadNames();
-});
+let tasks = []
+let taskId = 1
+const taskNameInput = document.getElementById('taskName')
+const addBt = document.getElementById('addBt')
+const container = document.getElementById('container')
 
-function loadNames(){
-  const names = JSON.parse(localStorage.getItem('names')) || [];
-  const nameList = document.getElementById('nameList');
+addBt.addEventListener('click', () => {
+  const newTask = {
+    id: taskId++,
+    name: taskNameInput.value
+  }
+  taskNameInput.value = ''
+  tasks.push(newTask)
+  // render()
+  console.log(tasks)
+})
 
-  names.forEach(name => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-      <span>${name}</span>
-      <button onclick="editName(this)">Редактировать</button>
-      <button onclick="deleteName(this)">Удалить</button>
-      <input type="checkbox" id="box" name="box">Выполнено
-      `;
-    nameList.appendChild(li);
-  });
-  filterNames();
-  // filterAcc()
-}
-
-function addName(){
-  const nameInput = document.getElementById('nameInput');
-  const nameText = nameInput.value.trim();
-
-  if(nameText === "") {
-    alert("Название задачи не может быть пустым")
-  } else {
-  const nameList = document.getElementById('nameList');
-  const li = document.createElement('li');
-  li.innerHTML = `
-      <span>${nameText}</span>
-      <button onclick="editName(this)">Редактировать</button>
-      <button onclick="deleteName(this)">Удалить</button>
-      <input type="checkbox" id="box" name="box">Выполнено
-      `;
-    nameList.appendChild(li);
-    saveNames();
-    nameInput.value="";
-    filterNames();}
-    // filterAcc()
-}
-
-function saveNames(){
-  const nameList = document.getElementById('nameList');
-  const names = Array.from(nameList.children).map(li=>li.querySelector('span').textContent);
-  localStorage.setItem('names', JSON.stringify(names));
-}
-
-function deleteName(button){
-  const li = button.parentElement;
-  li.remove();
-  saveNames();
-  filterNames();
-  // filterAcc()
-}
-
-function editName(button){
-  const li = button.parentElement;
-  const nameText = li.querySelector('span').textContent;
-  const newNameText = prompt("Редактировать задачу", nameText);
-
-  if(newNameText !== null && newNmaeText.trim() !==""){
-    li.querySelector('span').textContent = newNameText.trim();
-    saveNames();
-    filterNames();
-    // filterAcc()
-    }
-}
-
-function filterNames(){
-  const filterInput = document.getElementById('filterInput');
-  const filterText = filterInput.value.toLowerCase();
-  const nameList = document.getElementById('nameList');
-
-  Array.from(nameList.children).forEach(li=>{
-    const nameText = li.querySelector('span').textContent.toLowerCase();
-    if (nameText.includes(filterText)){
-      li.style.display = "";
-    }
-    else {
-      li.style.display = 'none';
-    }
+const render = () => {
+  container.innerHTML = ''
+  tasks.map((task) => {
+    const taskDiv = document.createElement("div")
+    taskDiv.innerText = task.name
+    container.appendChild(taskDiv)
   })
-}
-
-function filterAcc(){
-  // const filterAccept = document.getElementById('filterAccept');
-  // const filterVal = filterAccept.value.true();
-  const nameList = document.getElementById('nameList');
-  
-    Array.from(nameList.children).forEach(li=>{
-      const filterVal = li.querySelector('input');
-      if (filterVal === true){
-        li = "";
-      }
-      else {
-        li.style.display = Array;
-      }
-    })
 }
